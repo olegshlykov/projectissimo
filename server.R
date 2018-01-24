712febd17ba14b9597850dc2ddaf35dd8bf413bc
Oleh Shlykov <oleg.shlykov96@gmail.com>
2018-01-24 12:05:28 +0200

    Shifted sidebar to main panel Added Header, Quotes, Separator widgets Interface adjustments on POOSTAYA tab baby

--- server.R
+++ server.R
@@ -1,18 +1,20 @@
 server <- function(input, output) {
-
   output$textfile <- DT::renderDataTable({
-    
     req(input$file1)
     
-    df <- read.csv(input$file1$datapath)
+    df <- read.csv(input$file1$datapath,
+                   header = input$header,
+                   sep = input$sep,
+                   quote = input$quo)
     
   })
   punka <- reactiveValues()
   observeEvent(input$update, {
-     punka$a <- subset(mtcars, hp <= input$hrspwr & cyl %in% input$cylinders)
+    punka$a <-
+      subset(mtcars, hp <= input$hrspwr & cyl %in% input$cylinders)
   })
   output$cars <- DT::renderDataTable({
-    if (input$update!=0) {
+    if (input$update != 0) {
       punka$a
       
     } else {
--- ui.R
+++ ui.R
@@ -1,36 +1,70 @@
 library(shiny)
 library(DT)
 
-ui <- fluidPage(
-  
-  titlePanel("Uploading a CSV"),
-  
-  sidebarLayout(
-
-    sidebarPanel(
-      
-      fileInput("file1", "Please choose a CSV file:", 
-                multiple = FALSE,
-                accept = c("text/csv", 
-                           "text/comma-separated-values,text/plain", 
-                           ".csv")),
-      width = 3
-    ),
-    
-    mainPanel(
-      tabsetPanel(
-        tabPanel("Data Table", 
-                 dataTableOutput("textfile")),
-        tabPanel("Poostaya", 
-                 sliderInput("hrspwr", "Horsepower", min = 51, max = 335, value = 335),
-                 checkboxGroupInput("cylinders", "Cylinders", c("4", "6", "8"), selected = c("4", "6", "8")),
-                 checkboxInput("quo", "Quotes"),
-                 actionButton("update", "Update"),
-                 
-                 dataTableOutput("cars"))
-      )
-      
-      
-    )
-  )
-)
+ui <- fluidPage(titlePanel("Uploading a CSV"),
+                
+                tabsetPanel(
+                  tabPanel("Data Table",
+                           
+                           sidebarLayout(
+                             sidebarPanel(
+                               fileInput(
+                                 "file1",
+                                 "Please choose a CSV file:",
+                                 multiple = FALSE,
+                                 accept = c("text/csv",
+                                            "text/comma-separated-values,text/plain",
+                                            ".csv")
+                               ),
+                               tags$hr(),
+                               checkboxInput("header", "Header", TRUE),
+                               radioButtons("sep", "Separators", 
+                                            choices = c("Comma" = ",", "Semi" = ";", "Tab" = "\t"),
+                                            selected = ","),
+                               radioButtons(
+                                 "quo",
+                                 "Quote",
+                                 choices = c(
+                                   "None" = "",
+                                   "Double Quote" = '"',
+                                   "Single Quote" = "'"),
+                                   selected = '"'),
+                               width = 3
+                             ),
+                             
+                             mainPanel(dataTableOutput("textfile"))
+                           )),
+                  
+                  tabPanel(
+                    "Poostaya",
+                    fluidRow(
+                      column(
+                        4,
+                        offset = 1,
+                        sliderInput(
+                          "hrspwr",
+                          "Horsepower",
+                          min = 51,
+                          max = 335,
+                          value = 335
+                        )
+                      ),
+                      
+                      column(
+                        1,
+                        checkboxGroupInput(
+                          "cylinders",
+                          "Cylinders",
+                          c("4", "6", "8"),
+                          selected = c("4", "6", "8")
+                        )
+                      ),
+                      
+                      column(2, wellPanel(
+                             actionButton("update", "Update"), align = "center")
+                    )),
+                    
+                    fluidRow(column(10, offset = 1,
+                                    dataTableOutput("cars")))
+                  )
+                ))
